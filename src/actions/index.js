9c84091212dffda6cc4d05b3c13487d00e556cfd
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-05-06 12:37:05 -0700

    actions/index.js done for now, not too bad actually, all very similar, just dispatching actions after an asynch api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 // keys for actiontypes
 export const ActionTypes = {
+  // Think of as FETCH_POSTS_SUCCEEDED
   FETCH_POSTS: 'FETCH_POSTS',
   FETCH_POST: 'FETCH_POST',
-  // UPDATE_POST: 'UPDATE_POST',
-  // CREATE_POST: 'CREATE_POST',
-  // DELETE_POST: 'DELETE_POST',
-  INCREMENT: 'INCREMENT',
-  DECREMENT: 'DECREMENT',
+  ERROR_SET: 'ERROR_SET',
 };
 
 const ROOT_URL = 'https://platform.cs52.me/api';
 const API_KEY = '?key=sudharsan_balasubramani';
+/// IMPORTANT! API CALLS ONLY IN HERE, NOWHERE ELSE
+
+// Learned about axios calls from https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
 
 // We want a function that gets all posts
-export function fetchAll() {
+export function fetchPosts() {
   return (dispatch) => {
     // here is where we do asynch axios calls
     // on completion of which we dispatch a new action, we can dispatch stuff now
@@ -24,12 +24,55 @@ export function fetchAll() {
       dispatch({ type: ActionTypes.FETCH_POSTS, payload: response.data });
     }).catch((error) => {
       // hit an error do something else!
-      console.log(error);
+      dispatch({ type: ActionTypes.ERROR_SET, error });
     });
   };
 }
 
-/// IMPORTANT! API CALLS ONLY IN HERE, NOWHERE ELSE
+export function createPost(post, history) {
+  /* axios post */
+  return (dispatch) => {
+    axios.post(`${ROOT_URL}/posts${API_KEY}`, post).then((response) => {
+      // I think this is all we need to do? Do we need to refetch the posts?
+      history.push('/');
+    }).catch((error) => {
+      dispatch({ type: ActionTypes.ERROR_SET, error });
+    });
+  };
+}
+
+export function updatePost(id, fields) {
+  /* axios put */
+  return (dispatch) => {
+    axios.put(`${ROOT_URL}/posts/${id}${API_KEY}`, fields).then((response) => {
+      dispatch({ type: ActionTypes.FETCH_POST, payload: response.data });
+    }).catch((error) => {
+      dispatch({ type: ActionTypes.ERROR_SET, error });
+    });
+  };
+}
+
+export function fetchPost(id) {
+  /* axios get */
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`).then((response) => {
+      dispatch({ type: ActionTypes.FETCH_POST, payload: response.data });
+    }).catch((error) => {
+      dispatch({ type: ActionTypes.ERROR_SET, error });
+    });
+  };
+}
+
+export function deletePost(id, history) {
+  /* axios delete */
+  return (dispatch) => {
+    axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`).then((response) => {
+      history.push('/');
+    }).catch((error) => {
+      dispatch({ type: ActionTypes.ERROR_SET, error });
+    });
+  };
+}
 
 export function increment() {
   return {

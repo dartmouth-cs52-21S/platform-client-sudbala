9a2b62df51c538fb94b3d5f3c1e0336b1a6e63a8
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-05-06 11:52:47 -0700

    init comit with some edits to actions/index.js with axios. Lol nothing works yet, just exploring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,29 @@
+import axios from 'axios';
+
 // keys for actiontypes
 export const ActionTypes = {
+  FETCH_POSTS: 'FETCH_POSTS',
+  FETCH_POST: 'FETCH_POST',
+  // UPDATE_POST: 'UPDATE_POST',
+  // CREATE_POST: 'CREATE_POST',
+  // DELETE_POST: 'DELETE_POST',
   INCREMENT: 'INCREMENT',
   DECREMENT: 'DECREMENT',
 };
+
+const ROOT_URL = 'https://platform.cs52.me/api';
+const API_KEY = '?key=sudharsan_balasubramani';
+
+// We want a function that gets all posts
+export function fetchAll() {
+  axios.get(`${ROOT_URL}/posts${API_KEY}`).then((response) => {
+    // do something with response.data  (some json)
+  }).catch((error) => {
+    // hit an error do something else!
+  });
+}
+
+/// IMPORTANT! API CALLS ONLY IN HERE, NOWHERE ELSE
 
 export function increment() {
   return {
